f070c13234741a9a914e67a10a1bf88fd237d954
marktevin <myemail@gmail.com>
2023-02-20 08:48:15 -0800

    7-print_tebahpla

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -10,11 +10,12 @@
 int main(void)
 
 {
-	int d;
+	char low;
 
-	for (d = '0'; d <= '9'; d++)
-	putchar(d);
+	for (low = 'z'; low >= 'a'; low--)
+	putchar(low);
 	putchar('\n');
 
 	return (0);
+
 }
