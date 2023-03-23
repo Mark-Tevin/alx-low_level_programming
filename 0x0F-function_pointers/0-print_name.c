a638991395df32e0e8c5eb2832793ceb672f077c
marktevin <myemail@gmail.com>
2023-03-22 19:14:10 -0700

    0x0F. C - Function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+##include "function_pointers.h"
+
+/**
+ * print_name - print names using pointer to function
+ * @name - string to add
+ * @f: ponter to function
+ * Return : nothing
+ */
+
+void print _name(char *name, void (*f) (char *f))
+{
+	if (f! = NULL)
+		f(name);
+}
