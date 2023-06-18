cf725f9ce78453dcc86afb15546d3a0bf2189208
Mark-Tevin <tevinsmark@gmail.com>
2023-06-17 20:40:19 -0700

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -4,18 +4,18 @@
 /**
  * *_memset - fills memory with a constant byte
  * @s: memory area to be filled
- * @c: char to copy
+ * @b: char to copy
  * @n: number of times to copy b
  *
  * Return: pointer to the memory area s
  */
-char *_memset(char *s, char c, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
-		s[i] = c;
+		s[i] = b;
 	}
 
 	return (s);
