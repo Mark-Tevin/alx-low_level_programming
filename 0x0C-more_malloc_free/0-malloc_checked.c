43d45f90f338e927256f94725b86782f743d6f80
Mark-Tevin <tevinsmark@gmail.com>
2023-06-17 20:29:44 -0700

    more malloc qn 1

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,19 +3,18 @@
 
 /**
  * *malloc_checked - allocates memory using malloc
- * @a: number of bytes to allocate
+ * @b: number of bytes to allocate
  *
  * Return: a pointer to the allocated memory
  */
-void *malloc_checked(unsigned int a)
+void *malloc_checked(unsigned int b)
 {
 	void *ptr;
 
-	ptr = malloc(a);
+	ptr = malloc(b);
 
 	if (ptr == NULL)
 		exit(98);
 
 	return (ptr);
 }
-
