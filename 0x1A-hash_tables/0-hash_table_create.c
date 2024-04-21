61587e28fb5b34dc278f0bf23a1d0fbff028a05a
Mark-Tevin <tevinsmark@gmail.com>
2024-04-20 20:13:48 -0700

    creating a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,4 +9,21 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table;
+	hash_table_t *htable;
+
+	if (size == 0)
+		return (NULL);
+
+	htable = calloc(1, sizeof(hash_table_t));
+	if (htable == NULL)
+		return (NULL);
+
+	htable->size = size;
+	htable->array = calloc(size, sizeof(hash_node_t *));
+	if (htable->array == NULL)
+	{
+		free(htable);
+		return (NULL);
+	}
+	return (htable);
+}
